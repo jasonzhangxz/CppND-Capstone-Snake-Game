706ae13058998c6b129fb6c74ede6dcbc82d161e
Jason <jason.zhang@zenuity.com>
2020-05-16 08:50:21 -0400

    Add new class ScreenReward

---
+++ src/ScreenReward.cpp
@@ -0,0 +1,46 @@
+#include <random>
+#include "ScreenReward.h"
+
+ScreenReward::ScreenReward(std::size_t grid_width, std::size_t grid_height)
+  :ScreenObject(grid_width,grid_height)
+{
+    this->updatePos();
+}
+
+int ScreenReward::getTimeLeft(){
+  return timeLeft;
+}
+
+void ScreenReward::setPos(int x, int y){
+  pos.x = x;
+  pos.y = y;
+}
+void ScreenReward::run()
+{
+    // launch reward function in a thread
+    threads.emplace_back(std::thread(&ScreenReward::checkDuration, this));
+}
+
+void ScreenReward::updatePos(){
+  pos.x = random_w(engine);
+  pos.y = random_h(engine);
+  tStart = std::chrono::system_clock::now();
+  //show the reward on the screen for a random period (10-20s)
+  std::uniform_int_distribution<int> dist{5, 10};
+  expectDuration = dist(engine);
+  timeLeft = expectDuration;
+}
+
+void ScreenReward::checkDuration(){
+  while(true){
+    tNow = std::chrono::system_clock::now();
+    auto elapse = std::chrono::duration_cast<std::chrono::seconds>( tNow - tStart ).count();
+    // std::cout<<"DURATION IS: "<<duration<<std::endl;
+    timeLeft = expectDuration - elapse;
+    if ((timeLeft <= 0) && (pos.x > 0)){
+      setPos(-1,-1);//hide the reward Object
+    }
+    // sleep for 500ms between two cycles
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+}
---
+++ src/ScreenReward.h
@@ -0,0 +1,24 @@
+
+#ifndef SCREENREWARD_H
+#define SCREENREWARD_H
+
+#include <chrono>
+#include "ScreenObject.h"
+#include "SDL.h"
+
+class ScreenReward:public ScreenObject{
+public:
+  ScreenReward(std::size_t grid_width, std::size_t grid_height);
+  void run();
+  void updatePos();
+  SDL_Point pos;
+  int getTimeLeft();
+  void setPos(int x, int y);
+
+private:
+  std::chrono::system_clock::time_point tStart,tNow;
+  int expectDuration,timeLeft;
+  void checkDuration();
+};
+
+#endif
